80a13c458774cd11fee6c4c763c9ea06816554cf
Bartosz Piekarczyk <barpie02995@gmail.com>
2023-10-03 21:08:08 +0200

    change port

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Tworzenie obiektu Express
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,6 +19,6 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Serwer nasłuchuje na porcie 3000');
 });
